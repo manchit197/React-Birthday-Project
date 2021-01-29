e0d4960ccf9c006a12399178a41b63156f56812c
Jeff Suen <manchit197@gmail.com>
2021-01-29 21:18:47 +0800

    first commit

---
+++ src/Components/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import data from "../data";
+import List from "./List";
+
+function App() {
+  const [people, setPeople] = useState(data);
+
+  const clickHandler = () => {
+    setPeople([]);
+  };
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={clickHandler}>clear all</button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
